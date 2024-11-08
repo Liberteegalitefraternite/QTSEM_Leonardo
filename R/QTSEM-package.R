4524ca4975cb0230226c2f7e49f72b27c8782e61
Horan517 <haoranx517@gmail.com>
2024-11-08 09:28:49 -0500

    Initial commit

---
+++ R/QTSEM-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' packr: A package with a minimum example of package creation.
+#'
+#' This package is an exercise in package creation using
+#' R studio. The package includes a sample function and
+#' a sample dataset with their respective documentation.
+#'
+#' @docType package
+#' @name QTSEM-package
+#' @author Haoran Xu, School of Earth, Environment and Society, McMaster University \email{xu976@@mcmaster.ca}
+#' @references \url{https://github.com/paezha/packr}
+NULL
